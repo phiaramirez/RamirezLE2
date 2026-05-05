b75520496c5a6f6c026584b9e8a637499b13ca29
Phia Ramirez <phiaramirez05@gmail.com>
2026-05-06 04:11:14 +0800

    Final Lab 3 Submission

---
+++ RamirezBlogDB/Stored Procedures/SpUsers_Register.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[SpUsers_Register]
+	@userName nvarchar(16),
+	@firstName nvarchar(50),
+	@lastName nvarchar(50),
+	@password nvarchar(16)
+AS
+begin
+	set nocount on;
+
+	INSERT INTO dbo.Users (UserName, FirstName, LastName, Password)
+	VALUES (@userName, @firstName, @lastName, @password)
+end
